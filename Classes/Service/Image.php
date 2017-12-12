1ba3272dec9931e1739ad4155f32e33883999996
BochkarevAS <bochkarev-vl@mail.ru>
2017-12-12 12:17:58 +1000

    Init Commit

--- Classes/Service/Image.php
+++ Classes/Service/Image.php
@@ -64,15 +64,25 @@ class Image {
         $uploadfile = "$year/$month/$day/" . $this->makeSeed() . $ext;
 
         if (move_uploaded_file($file['tmp_name'], $uploaddir . $uploadfile)) {
-            $sql = "INSERT INTO image (img, uid) VALUES (:img, :uid) RETURNING id, img";
+//            Писал в начале под PostgreSQL
+//            $sql = "INSERT INTO image (img, uid) VALUES (:img, :uid) RETURNING id, img";
+
+            $sql = "INSERT INTO image (img, uid) VALUES (:img, :uid)";
             $result = $db->prepare($sql);
 
             $result->bindParam('img', $uploadfile, PDO::PARAM_STR);
             $result->bindParam('uid', $uid, PDO::PARAM_STR);
             $result->execute();
-
-            $img = $result->fetch();
         }
+
+        $sql = "SELECT img FROM image WHERE img = :img AND uid = :uid";
+        $result = $db->prepare($sql);
+
+        $result->bindParam('img', $uploadfile, PDO::PARAM_STR);
+        $result->bindParam('uid', $uid, PDO::PARAM_STR);
+        $result->execute();
+
+        $img = $result->fetch();
 
         return $img;
     }
